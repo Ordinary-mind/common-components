54c00331e193a41aa5f3b54fc8e719e420304850
Ordinary-mind <1633263519@qq.com>
2021-04-29 09:33:34 +0800

    v0.1.1版本

---
+++ examples/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <custom-text text="hello lqn"></custom-text>
+    <custom-text text="真是个悲伤的故事"></custom-text>
+    <custom-list :list="list1"></custom-list>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import myComponents from '../packages/index'
+Vue.use(myComponents)
+export default {
+  name: 'app',
+  data: function(){
+    return {
+    name: "lqn",
+    list1: [{name: "aaaaaaaaaaa"},{name: "bbbbbbbbbbbbb"}]
+  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/custom-list/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>这是一个列表组件</h1>
+        <ul>
+            <li v-for="(item,index) in list" :key="index">
+                <span>{{index}}</span>
+                <span>{{item.name}}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: "customList",
+    props: ['list']
+}
+</script>
